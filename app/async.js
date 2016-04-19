d50c864648dda99bac4e4c24909d9bb5be85af99
Fabien Thebaud <fthebaud@gmail.com>
2016-04-19 23:02:55 +0200

    async ok

--- app/async.js
+++ app/async.js
@@ -1,11 +1,32 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
-  async : function(value) {
-
+  async: function(value) {
+    return new Promise(function(resolve, reject) {
+      resolve(value);
+    });
   },
 
-  manipulateRemoteData : function(url) {
+  manipulateRemoteData: function(url) {
+    return new Promise(function(resolve, reject) {
+      var client = new XMLHttpRequest();
+      client.open('GET', url);
+      client.send();
 
+      client.onload = function() {
+        if (this.status >= 200 && this.status < 300) {
+          var resultat = JSON.parse(this.response).people.map(function(a) {
+            return a.name;
+          });
+          resolve(resultat.sort());
+        } else {
+          reject(this.statusText);
+        }
+      };
+
+      client.onerror = function() {
+        reject(this.statusText);
+      };
+    });
   }
 };
