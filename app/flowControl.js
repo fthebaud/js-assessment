e21bc99210a3ba3e3bdf13811bff83c26a816cf8
Fabien Thebaud <fthebaud@gmail.com>
2016-04-20 15:01:45 +0200

    best practice, count, flow control ok

--- app/flowControl.js
+++ app/flowControl.js
@@ -1,7 +1,29 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.flowControlAnswers = {
-  fizzBuzz : function(num) {
+  fizzBuzz: function(num) {
+    if (num && typeof(num) === 'number') {
+      var res = num;
+
+      if (num % 3 === 0) {
+        res = 'fizz';
+      }
+      
+      if (num % 5 === 0) {
+        if (res === 'fizz') {
+          res += 'buzz';
+        }
+        else {
+          res = 'buzz';
+        }
+      }
+      return res;
+    }
+    else {
+      return false;
+    }
+
+
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
@@ -10,6 +32,5 @@ exports.flowControlAnswers = {
     //
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
-
   }
 };
